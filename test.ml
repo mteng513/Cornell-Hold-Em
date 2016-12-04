ab7c98e66919ee3c666cc7884d628e2af818bd71
Will Ronchetti <wrr33@cornell.edu>
2016-12-04 15:51:49 -0500

    added a lot more tests

--- test.ml
+++ test.ml
@@ -5,6 +5,15 @@ open Player
 open Opponent
 open GUpoker
 
+(* This is our OUnit test file for the Entire System. Many aspects of 
+ * this system need to be play tested, particularly the opponent and
+ * betting mechanisms. Most unit tests have to do with scoring and
+ * updating state properly, as these are the things that make the most
+ * sense to run OUnit tests on. We test individual scoring functionality
+ * then the overall scoring capability and then test that the scoring
+ * is properly reflected in the state. We also test some basic state
+ * transitions. Most tests tell what they're testing in the name.
+ * We've defined some basic states for testing purposes at the top. *)
 
 module Test_Game_Engine = struct
 
@@ -20,7 +29,7 @@ module Test_Game_Engine = struct
 		bets = [|0;0;0;0;0;0;0;0|]; 
 		players_in = [|true; true; true; false; false; false; false; false|];
 		scores = [|0;0;0;0;0;0;0;0|];
-		chips = [|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
+		chips =[|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
 
 	let test_state_after = {GEngine.current_st = START; 
 		cards_in_play = [(Ace, Gries); (Two, Gries);
@@ -32,19 +41,19 @@ module Test_Game_Engine = struct
  		bets = [|0; 0; 0; 0; 0; 0; 0; 0|];
  		players_in = [|true; true; true; false; false; false; false; false|];
  		scores = [|1000000000; 1401312; 2322; 0; 0; 0; 0; 0|];
- 		chips = [|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
+ 		chips =[|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
 
  	let test_b = {GEngine.current_st = START;                                                                                                                                                       
-   		cards_in_play = [(Nine, George); (Ace, Dijkstra);
-   		(Three, Clarkson); (Eight, Dijkstra); (Jack, Gries)];
-   		pot = 100000; 
-   		current_bet = 0; n_players = 0; c_player = 0;
-   		hands = [[(Ten, Gries); (Seven, Gries)];
-   			[(King, Clarkson); (Ace, Clarkson)]];
-   		bets = [|0; 0; 0; 0; 0; 0; 0; 0|];
-   		players_in = [|true; true; false; false; false; false; false; false|];
-   		scores = [|0; 0; 0; 0; 0; 0; 0; 0|];
-   		chips = [|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
+   	cards_in_play = [(Nine, George); (Ace, Dijkstra);
+   	(Three, Clarkson); (Eight, Dijkstra); (Jack, Gries)];
+   	pot = 100000; 
+   	current_bet = 0; n_players = 0; c_player = 0;
+   	hands = [[(Ten, Gries); (Seven, Gries)];
+   		[(King, Clarkson); (Ace, Clarkson)]];
+   	bets = [|0; 0; 0; 0; 0; 0; 0; 0|];
+   	players_in = [|true; true; false; false; false; false; false; false|];
+   	scores = [|0; 0; 0; 0; 0; 0; 0; 0|];
+   	chips =[|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
 
    	let test_b_after = {GEngine.current_st = START; 
    		cards_in_play = [(Nine, George); (Ace, Dijkstra);
@@ -56,7 +65,7 @@ module Test_Game_Engine = struct
  		bets = [|0; 0; 0; 0; 0; 0; 0; 0|];
  		players_in = [|true; true; false; false; false; false; false; false|];
  		scores = [|15000011; 22419; 0; 0; 0; 0; 0; 0|];
- 		chips = [|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
+ 		chips =[|9999999;9999999;9999999;9999999;9999999;9999999;9999999;9999999|]}
 
 	let tests = "test suite" >::: [
 
@@ -188,8 +197,8 @@ module Test_Game_Engine = struct
 
 		"Pair: King (and Two)" >:: (fun _ -> assert_equal 21023 
 			(Game_Engine.pair
-			[(Ace, Gries); (Two, Gries); (Three, Clarkson); (Queen, Gries); (King, Gries);
-	        (King, Clarkson); (Two, Clarkson)]));
+			[(Ace, Gries); (Two, Gries); (Three, Clarkson); (Queen, Gries); 
+				(King, Gries); (King, Clarkson); (Two, Clarkson)]));
 
 		"Pair: Twos (3 of a kind) has Six" >:: (fun _ -> assert_equal 4426 
 			(Game_Engine.pair
@@ -268,26 +277,206 @@ module Test_Game_Engine = struct
 			[(Ace, Gries); (Queen, Gries); (Jack, Gries); (Ace, Clarkson); 
 			 (King, Gries); (Ten, Gries); (Two, Clarkson)]));
 
+		"Score Calc: Royal Flush (Ordering changes)" 
+			>:: (fun _ -> assert_equal 1000000000 
+			(Game_Engine.score_calculation
+			[(Ten, Gries); (Jack, Gries); (Queen, Gries); (Two, Clarkson); 
+			 (King, Gries); (Ace, Gries); (Five, Clarkson)]));
+
 		"Score Calc: High Card (no hand)" >:: (fun _ -> assert_equal 2224 
 			(Game_Engine.score_calculation
 			[(Three, Gries); (Two, Gries); (Jack, George); (Ace, Clarkson); 
 			 (Seven, George); (Ten, Dijkstra); (Four, Clarkson)]));
 
+		"Score Calc: High Card (higher tiebreaker)" >:: (fun _ -> assert_equal 2225
+			(Game_Engine.score_calculation
+			[(Four, Gries); (Three, Gries); (Jack, George); (Ace, Clarkson); 
+			 (Seven, George); (Ten, Dijkstra); (Five, Clarkson)]));
+
 		"Score Calc: Regular flush" >:: (fun _ -> assert_equal (16120643)
 	    (GEngine.score_calculation [(Ten, Gries); (Three, Gries); (Four, Gries); 
 	    	(Five, Clarkson); (Six, Gries); (Jack, Gries)]));
+
+		"Score Calc: Regular flush with a Pair" >:: 
+		(fun _ -> assert_equal (16120643)
+	    (GEngine.score_calculation [(Ten, Gries); (Three, Gries); (Four, Gries); 
+	    	(Four, Clarkson); (Six, Gries); (Jack, Gries)]));
+
+		"Score Calc: Regular flush with 2 Pair" >:: 
+			(fun _ -> assert_equal (16120643)
+	    (GEngine.score_calculation [(Ten, Gries); (Three, Gries); (Four, Gries);
+	    	(Four, Clarkson); (Six, Gries); (Jack, Gries); (Three, Dijkstra)]));
+
+			"Score Calc: Regular flush with triple" >:: 
+			(fun _ -> assert_equal (16120643)
+	    (GEngine.score_calculation [(Ten, Gries); (Three, Gries); (Four, Gries); 
+	    	(Four, Clarkson); (Six, Gries); (Jack, Gries); (Four, Dijkstra)]));
+
+		"Score Calc: Higher flush" >:: (fun _ -> assert_equal (16120654)
+	    (GEngine.score_calculation [(Ten, Gries); (Four, Gries); (Five, Gries); 
+	    	(Five, Clarkson); (Six, Gries); (Jack, Gries)]));
+
+		"Score Calc: A straight flush"  >:: (fun _ -> assert_equal 20000006
+			(GEngine.straight [(Two, Gries); (Three, Gries); (Four, Gries); 
+				(Five, Gries); (Six, Gries); (Jack, Dijkstra)]));
+
+		"Score Calc: A higher straight flush"  >:: (fun _ -> assert_equal 20000007
+			(GEngine.straight [(Three, Gries); (Four, Gries); (Five, Gries); 
+				(Six, Gries); (Seven, Gries); (Jack, Dijkstra)]));
 
 		"Score Calc: Regular Straight" >:: (fun _ -> assert_equal 15000006
 		(GEngine.score_calculation [(Two, Gries); (Three, Gries); (Four, Gries); 
 			(Five, Clarkson); (Six, Gries); (Jack, Dijkstra)]));
 
+		"Score Calc: Regular Straight with pair" >:: 
+		(fun _ -> assert_equal 15000006
+		(GEngine.score_calculation [(Two, Gries); (Three, Gries); (Four, Gries); 
+			(Five, Clarkson); (Six, Gries); (Jack, Dijkstra); (Two, Dijkstra)]));
 
+		"Score Calc: Regular Straight with 2pair" >:: 
+		(fun _ -> assert_equal 15000006
+		(GEngine.score_calculation [(Two, Gries); (Three, Gries); (Four, Gries); 
+			(Five, Clarkson); (Six, Gries); (Six, Dijkstra); (Two, Dijkstra)]));
 
+		"Score Calc: Regular Straight with triple" >:: 
+		(fun _ -> assert_equal 15000006
+		(GEngine.score_calculation [(Two, Gries); (Three, Gries); (Four, Gries); 
+			(Five, Clarkson); (Six, Gries); (Six, Dijkstra); (Six, George)]));
 
+		"Score Calc: Regular Straight (Sanity check for ordering)" >:: 
+		(fun _ -> assert_equal 15000006
+		(GEngine.score_calculation [(Three, Gries); (Four, Gries); (Two, Gries); 
+			(Jack, Clarkson); (Six, Gries); (Five, Dijkstra)]));
+
+		"Score Calc: Higher Straight" >:: (fun _ -> assert_equal 15000007
+		(GEngine.score_calculation [(Three, Gries); (Four, Gries); (Five, Gries); 
+			(Six, Clarkson); (Seven, Gries); (Jack, Dijkstra)]));
+
+		"Score Calc: Complex High Card" >:: (fun _ -> assert_equal 1413 
+			(GEngine.score_calculation [(Three, Gries); (Four, Clarkson); 
+				(Six, Gries); (Seven, Clarkson); (Nine, Dijkstra)]));
+
+		"Score Calc: Complex lower High Card" >:: (fun _ -> assert_equal 1412 
+			(GEngine.score_calculation [(Two, Gries); (Four, Clarkson); (Six, Gries); 
+				(Seven, Clarkson); (Nine, Dijkstra)]));
+
+		"Score Calc: Full House" >:: (fun _ -> assert_equal 18004014 
+			(GEngine.score_calculation [(Four, Gries); (Four, Clarkson); 
+				(Four, Gries); (Ace, Clarkson); (Ace, Dijkstra)]));
+
+		"Score Calc: Full House with 2 triples" >:: (fun _ -> assert_equal 18014004
+			(GEngine.score_calculation [(Four, Gries); (Four, Clarkson); 
+				(Four, Gries);(Ace, Clarkson); (Ace, Dijkstra); (Ace, George)]));
+
+		"Score Calc: Lower Full House" >:: (fun _ -> assert_equal 18004010 
+			(GEngine.score_calculation [(Four, Gries); (Four, Clarkson); 
+				(Four, Gries);(Ten, Clarkson); (Ten, Dijkstra)]));
+
+		"Score Calc: Higher Full House" >:: (fun _ -> assert_equal 18005014 
+			(GEngine.score_calculation [(Five, Gries); (Five, Clarkson); 
+				(Five, Gries); (Ace, Clarkson); (Ace, Dijkstra)]));
+
+		"Score Calc: Two Pair: Ace, King" >:: (fun _ -> assert_equal 1401312 
+			(Game_Engine.score_calculation [(Ace,Gries); (Two, Gries); 
+				(Three, Clarkson); (Queen, Gries); (King, Gries);(King, Clarkson);
+				(Ace, Clarkson)]));
+
+		"Score Calc: Three Pair: Ace, King, three" >:: 
+			(fun _ -> assert_equal 1401312 
+			(Game_Engine.score_calculation [(Ace,Gries); (Three, Gries); 
+				(Three, Clarkson); (Queen, Gries); (King, Gries);(King, Clarkson);
+				(Ace, Clarkson)]));
+
+		"Score Calc: Two Pair: Ace, King (Sanity check for ordering)" >:: 
+			(fun _ -> assert_equal 1401312 (Game_Engine.score_calculation 
+				[(Queen,Gries); (Ace, Gries);(King, Clarkson); (Ace, Gries); 
+				(King, Gries);(Three, Clarkson);(Two, Clarkson)]));
+
+		"Score Calc: Two Pair: Ace, King with lower tie breaker" >:: 
+			(fun _ -> assert_equal 1401311 (Game_Engine.score_calculation 
+				[(Ace,Gries); (Two, Gries); (Three, Clarkson); (Jack, Gries); 
+				(King, Gries);(King, Clarkson);(Ace, Clarkson)]));
+
+		"Score Calc: Pair Ace" >:: (fun _ -> assert_equal 22431 
+			(Game_Engine.score_calculation
+			[(Ace, Gries); (Two, Gries); (Three, Clarkson);(Queen, Gries); 
+			(King, Gries); (Jack, Clarkson); (Ace, Clarkson)]));
+
+		"Score Calc: Pair Ace with lower tie breaker" >:: 
+			(fun _ -> assert_equal 22430 (Game_Engine.score_calculation
+			[(Ace, Gries); (Two, Gries); (Three, Clarkson);(Queen, Gries); 
+			(King, Gries); (Ten, Clarkson); (Ace, Clarkson)]));
+
+		"Score Calc Pair King" >:: (fun _ -> assert_equal 21025 
+			(Game_Engine.score_calculation
+			[(Ace, Gries); (Two, Gries); (Three, Clarkson); 
+			(Queen, Gries); (King, Gries); (King, Clarkson); (Five, Clarkson)]));
+
+		"Score Calc Pair King with lower tie breaker" >:: 
+		  (fun _ -> assert_equal 21024 (Game_Engine.score_calculation
+			[(Ace, Gries); (Two, Gries); (Three, Clarkson); 
+			(Queen, Gries); (King, Gries); (King, Clarkson); (Four, Clarkson)]));
+
+		"Score Calc: Three Kind Two" >:: (fun _ -> assert_equal 2001312 
+			(Game_Engine.score_calculation
+			[(Two, George); (Two, Gries); (Three, Clarkson); (Queen, Gries);
+			 (King, Gries); (Four, Clarkson); (Two, Clarkson)]));
+
+		"Score Calc: Three Kind Two with lower tie breaker" >:: 
+			(fun _ -> assert_equal 2001311 (Game_Engine.score_calculation
+			[(Two, George); (Two, Gries); (Three, Clarkson); (Jack, Gries);
+			 (King, Gries); (Four, Clarkson); (Two, Clarkson)]));
+
+		"Score Calc: Three Kind Four" >:: (fun _ -> assert_equal 4001307 
+			(Game_Engine.score_calculation
+			[(Four, George); (Four, Gries); (Four, Clarkson); (Seven, Gries);
+			 (King, Gries); (Three, Dijkstra); (Two, Clarkson)]));
+
+		"Score Calc: Three Kind Ace" >:: (fun _ -> assert_equal 14001307 
+			(Game_Engine.score_calculation
+			[(Ace, George); (Ace, Gries); (Four, Clarkson); (Seven, Gries);
+			 (King, Gries); (Ace, Dijkstra); (Two, Clarkson)]));
+
+		"Score Calc: Four Kind Four" >:: (fun _ -> assert_equal 19004013 
+			(Game_Engine.score_calculation
+			[(Four, George); (Four, Gries); (Four, Clarkson); (Seven, Gries);
+			 (King, Gries); (Four, Dijkstra); (Two, Clarkson)]));
+
+		"Score Calc: Four Kind Four with full house" >:: 
+			(fun _ -> assert_equal 19004013 (Game_Engine.score_calculation
+			[(Four, George); (Four, Gries); (Four, Clarkson); (Seven, Gries);
+			 (King, Gries); (Four, Dijkstra); (King, Clarkson)]));
+
+		"Score Calc: Four Kind Four with Triple" >:: 
+			(fun _ -> assert_equal 19004013 (Game_Engine.score_calculation
+			[(Four, George); (Four, Gries); (Four, Clarkson); (King, Dijkstra);
+			 (King, Gries); (Four, Dijkstra); (King, Clarkson)]));
+
+		"Score Calc: Four Kind Four (Sanity check for order changes)" >:: 
+			(fun _ -> assert_equal 19004013 (Game_Engine.score_calculation
+			[(Four, George); (King, Gries); (Two, Clarkson); (Seven, Gries);
+			 (Four, Gries); (Four, Dijkstra); (Four, Clarkson)]));
+
+		"Score Calc: Four Kind Ace" >:: (fun _ -> assert_equal 19014013 
+			(Game_Engine.score_calculation
+			[(Ace, George); (Ace, Gries); (Four, Clarkson); (Seven, Gries);
+			 (King, Gries); (Ace, Dijkstra); (Ace, Clarkson)]));
+
+		"Score Calc: Four Kind Ace lower tie breaker" >:: 
+			(fun _ -> assert_equal 19014012 (Game_Engine.score_calculation
+			[(Ace, George); (Ace, Gries); (Four, Clarkson); (Seven, Gries);
+			 (Queen, Gries); (Ace, Dijkstra); (Ace, Clarkson)]));
+
+		"Four Kind: Ace (Sanity check for changed order)" 
+			>:: (fun _ -> assert_equal 19014013 
+			(Game_Engine.score_calculation
+			[(Ace, Dijkstra); (Ace, George); (Ace, Gries); (Ace, Clarkson); 
+			 (Seven, Gries); (King, Gries); (Two, Clarkson)])); 
 
 		(* END OF SCORE CALCULATION TESTS.
 		 * Now, we run a few tests to make sure state is properly updated
 		 * when scoring. *)
+
 		"Score 0a: Check that returns unit" >:: (fun _ -> assert_equal () 
 			(Game_Engine.score test_state));
 

c0e89a33b6372e56cabff7da5bf18afbf25ab10b
mgw55 <mgw55@cornell.edu>
2016-12-05 06:04:57 -0500

    improved AI decision making and randomness

--- opponent.ml
+++ opponent.ml
@@ -141,38 +141,68 @@ module Opponent = struct
         | George -> c4 := !c4 + 1
       done);
     four_max !c1 !c2 !c3 !c4  
+
+
+  let high_random_decider st chips =
+    if Random.bool () then int_of_float ((Random.float 200.) /. 250. *. chips)
+    else if Random.bool () 
+      then int_of_float ((Random.float 200.) /. 200. *. chips)
+    else if Random.bool () then (st.current_bet - (Array.get st.bets st.c_player)) 
+        else 0
+
+  let medium_random_decider st chips =
+    if Random.bool () then 
+      if Random.bool () then (st.current_bet - (Array.get st.bets st.c_player)) else 0
+    else 
+      if Random.bool () 
+      then int_of_float ((Random.float 200.) /. 350. *. chips) 
+      else (st.current_bet - (Array.get st.bets st.c_player))
+      (* else int_of_float ((Random.float 200.) /. 300. *. chips) *)
+
+  let low_random_decider st chips =
+    if Random.bool () then 0
+    else if Random.bool () 
+      then (st.current_bet - (Array.get st.bets st.c_player))
+    else int_of_float ((Random.float 200.) /. 450. *. chips)
+
+  let lower_random_decider st chips =
+    if Random.bool () then 0
+    else if Random.bool () 
+      then (st.current_bet - (Array.get st.bets st.c_player))
+    else int_of_float ((Random.float 200.) /. 500. *. chips)
  
   (* General helper for bets after the first. *)
-  let bet_helper st current_score cardlst = 
-
+  let bet_helper st current_score cards = 
+    let chips = float_of_int (Array.get st.chips st.c_player) in
     (* Royal Flush. Bet Everything *)
-    if (current_score >= 100000000) then (Random.int 750)
-
+    if (current_score >= 100000000) then int_of_float chips
     (* Straight flush. Bet everything *)
-    else if (current_score >= 20000005) then (Random.int 750)
-
+    else if (current_score >= 20000005) then int_of_float (chips)
     (* Four of a kind. Bet a good amount *)
-    else if (current_score >= 19002003) then (Random.int 500)
-
+    else if (current_score >= 19002003) then int_of_float (chips)
     (* Full House. Bet a decent amount. *)
-    else if (current_score >= 18002003) then (Random.int 300)
-
+    else if (current_score >= 18002003) then int_of_float (chips *. 0.95)
     (* Manual check for flush (if we are close, we should bet) *)
-    else if ((flush cardlst) >= 4) then (Random.int 200)
-
+    else if ((flush cards) >= 4) then high_random_decider st chips
     (* Straight. Bet a decent amount. *)
-    else if (current_score >= 15000002) then (Random.int 150)
-
+    else if (current_score >= 15000002) then high_random_decider st chips
+    (* 3 of a kind. Face cards. Bet a large amount *)
+    else if (current_score >= 10000000) then high_random_decider st chips
     (* 3 of a kind. Bet a decent amount *)
-    else if (current_score >= 2000000) then (Random.int 100)
-
+    else if (current_score >= 2000000) then medium_random_decider st chips
+    (* 2 pair. Face cards only. *)
+    else if (current_score >= 1000000) then medium_random_decider st chips
+    (* 2 pair. Average (7-10) cards only. *)
+    else if (current_score >= 600000) then medium_random_decider st chips
     (* 2 pair. Bet a little. *)
-    else if (current_score >= 200000) then (Random.int 50)
-
+    else if (current_score >= 200000) then low_random_decider st chips
+    (* Pair. Face cards only. *)
+    else if (current_score >= 17300) then low_random_decider st chips
+    (* Pair. Average (7-10) cards only. *)
+    else if (current_score >= 11200) then lower_random_decider st chips
     (* Pair. Bet a tiny bit. *)
-    else if (current_score >= 2000) then (Random.int 20)
-
-    else 0
+    else if (current_score >= 2000) then lower_random_decider st chips
+    else lower_random_decider st chips
 
     (* Sorts cards by RANK. *)
   let sort_cards (h:hand) : hand = 
@@ -181,109 +211,118 @@ module Opponent = struct
 
 
   let twofifty_bet st current_score cards = 
-
+    let chips = float_of_int (Array.get st.chips st.c_player) in
     (* Royal Flush. Bet Everything *)
-    if (current_score >= 100000000) then 9999999999999 
-
+    if (current_score >= 100000000) then int_of_float chips
     (* Straight flush. Bet everything *)
-    else if (current_score >= 20000005) then 9999999999999 
-
+    else if (current_score >= 20000005) then int_of_float (chips)
     (* Four of a kind. Bet a good amount *)
-    else if (current_score >= 19002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 19002003) then int_of_float (chips)
     (* Full House. Bet a decent amount. *)
-    else if (current_score >= 18002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 18002003) then int_of_float (chips *. 0.95)
     (* Manual check for flush (if we are close, we should bet) *)
-    else if ((flush cards) >= 4) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if ((flush cards) >= 4) then high_random_decider st chips
     (* Straight. Bet a decent amount. *)
-    else if (current_score >= 15000002) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 15000002) then high_random_decider st chips
+    (* 3 of a kind. Face cards. Bet a large amount *)
+    else if (current_score >= 10000000) then high_random_decider st chips
     (* 3 of a kind. Bet a decent amount *)
-    else if (current_score >= 2000000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 2000000) then medium_random_decider st chips
+    (* 2 pair. Face cards only. *)
+    else if (current_score >= 1000000) then medium_random_decider st chips
+    (* 2 pair. Average (7-10) cards only. *)
+    else if (current_score >= 600000) then medium_random_decider st chips
     (* 2 pair. Bet a little. *)
-    else if (current_score >= 200000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 200000) then medium_random_decider st chips
+    (* Pair. Face cards only. *)
+    else if (current_score >= 17300) then medium_random_decider st chips
+    (* Pair. Average (7-10) cards only. *)
+    else if (current_score >= 11200) then low_random_decider st chips
     (* Pair. Bet a tiny bit. *)
-    else if (current_score >= 2000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
-    else 0
+    else if (current_score >= 2000) then low_random_decider st chips
+    else lower_random_decider st chips
 
   let sixhun_bet st current_score cards = 
-
+    let chips = float_of_int (Array.get st.chips st.c_player) in
     (* Royal Flush. Bet Everything *)
-    if (current_score >= 100000000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    if (current_score >= 100000000) then int_of_float chips
     (* Straight flush. Bet everything *)
-    else if (current_score >= 20000005) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 20000005) then int_of_float (chips)
     (* Four of a kind. Bet a good amount *)
-    else if (current_score >= 19002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 19002003) then int_of_float (chips)
     (* Full House. Bet a decent amount. *)
-    else if (current_score >= 18002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 18002003) then int_of_float (chips *. 0.95)
     (* Manual check for flush (if we are close, we should bet) *)
-    else if ((flush cards) >= 4) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if ((flush cards) >= 4) then high_random_decider st chips
+    (* Straight. Facecards. Bet a high amount. *)
+    else if (current_score >= 20000000) then high_random_decider st chips
     (* Straight. Bet a decent amount. *)
-    else if (current_score >= 15000002) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 15000002) then medium_random_decider st chips
+    (* 3 of a kind. Face cards. Bet a large amount *)
+    else if (current_score >= 10000000) then medium_random_decider st chips
     (* 3 of a kind. Bet a decent amount *)
-    else if (current_score >= 2000000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 2000000) then medium_random_decider st chips
+    (* 2 pair. Face cards only. *)
+    else if (current_score >= 1000000) then medium_random_decider st chips
+    (* 2 pair. Average (7-10) cards only. *)
+    else if (current_score >= 600000) then medium_random_decider st chips
     (* 2 pair. Bet a little. *)
-    else if (current_score >= 200000) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 200000) then medium_random_decider st chips
+    (* Pair. Face cards only. *)
+    else if (current_score >= 17300) then low_random_decider st chips
+    (* Pair. Average (7-10) cards only. *)
+    else if (current_score >= 11200) then lower_random_decider st chips
     (* Pair. Bet a tiny bit. *)
-    else if (current_score >= 2000) then 0
-
-    else 0
+    else if (current_score >= 2000) then lower_random_decider st chips
+    else lower_random_decider st chips
 
   let thous_bet st current_score cards = 
-
+    let chips = float_of_int (Array.get st.chips st.c_player) in
     (* Royal Flush. Bet Everything *)
-    if (current_score >= 100000000) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    if (current_score >= 100000000) then int_of_float chips 
 
     (* Straight flush. Bet everything *)
-    else if (current_score >= 20000005) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    else if (current_score >= 20000005) then int_of_float chips 
 
     (* Four of a kind. Bet a good amount *)
-    else if (current_score >= 19002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    else if (current_score >= 19002003) then int_of_float chips 
 
     (* Full House. Bet a decent amount. *)
-    else if (current_score >= 18002003) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    else if (current_score >= 18002003) then high_random_decider st chips 
 
     (* Manual check for flush (if we are close, we should bet) *)
-    else if ((flush cards) >= 4) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    else if ((flush cards) >= 4) then medium_random_decider st chips 
 
     (* Straight. Bet a decent amount. *)
-    else if (current_score >= 15000002) then (st.current_bet - (Array.get st.bets st.c_player)) 
-
+    else if (current_score >= 15000002) then medium_random_decider st chips 
+    (* 3 of a kind. Face cards. Bet a large amount *)
+    else if (current_score >= 10000000) then low_random_decider st chips
     (* 3 of a kind. Bet a decent amount *)
-    else if (current_score >= 2000000) then (st.current_bet - (Array.get st.bets st.c_player)) 
+    else if (current_score >= 2000000) then lower_random_decider st chips 
 
     (* 2 pair. Bet a little. *)
-    else if (current_score >= 200000) then 0
+    else if (current_score >= 200000) then lower_random_decider st chips
 
     (* Pair. Bet a tiny bit. *)
-    else if (current_score >= 2000) then 0
+    else if (current_score >= 2000) then lower_random_decider st chips
 
+    else lower_random_decider st chips
+
+  let bluff_bet st = 
+    if Random.bool () then 
+      if Random.bool () then (st.current_bet - (Array.get st.bets st.c_player))
+      else 0
     else 0
-
-  let bluff_bet st = (st.current_bet - (Array.get st.bets st.c_player)) 
 
   (* Secondary decision function for the second round of betting. *)
   let decide_two st score cards = 
     let current_bet = st.current_bet in 
-
+    let chips = float_of_int (Array.get st.chips st.c_player) in
     if current_bet < 100 then current_bet 
     else if current_bet < 250 then (twofifty_bet st score cards) 
-    else if current_bet < 600 then sixhun_bet st score cards 
-    else if current_bet < 1000 then thous_bet st score cards  
-    else  bluff_bet st
+    else if current_bet < 950 then sixhun_bet st score cards 
+    else if current_bet < 2500 then thous_bet st score cards  
+    else bluff_bet st
 
   (* Main decision function for the opponent. *)
   let decide st score = 
